d3e8757e84d73f6273f1ab76772d50fadb1b82cb
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-08-31 19:50:31 +0200

    Update package init

--- app/material/__init__.py
+++ app/material/__init__.py
@@ -1,2 +1,3 @@
 from app.material.typography import Header, Title, SubHeader  # noqa: F401
 from app.material.admonition import Admonition  # noqa: F401
+from app.material.button import Button  # noqa: F401
